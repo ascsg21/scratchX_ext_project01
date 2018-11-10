dcb6ca728c025daf400642af49e8b8dfabf1c218
ascsg21 <lovelyroom77@gmail.com>
2018-11-10 15:35:35 +0900

    11월 10일

--- youtubeAPI.js
+++ youtubeAPI.js
@@ -20,44 +20,6 @@
       }
       var info = JSON.parse(Httpreq.responseText);
       callback(info.items[0].statistics.viewCount);
-
-      // $.ajax({
-      //     url: 'https://www.googleapis.com/youtube/v3/channels?part=id,snippet,brandingSettings,contentDetails,invideoPromotion,statistics,topicDetails&id=UCWs0WvjYOKdDmpW4At3hSag', // 요청 할 주소
-      //     dataType: 'jsonp',
-      //     async: false, // false 일 경우 동기 요청으로 변경
-      //     type: 'GET', // GET, PUT
-      //     // data: {
-      //     //     part: 'id,snippet,contentDetails',
-      //     //     channelId: 'UCWs0WvjYOKdDmpW4At3hSag'
-      //     // }, // 전송할 데이터
-      //     // beforeSend: function(data) {callback('aaa');}, // 서버 요청 전 호출 되는 함수 return false; 일 경우 요청 중단
-      //     success: function(data) {
-      //       // for( x in data.data ){
-      //   		// 	$('ul').append('<li><img src="'+data.data[x].images.low_resolution.url+'"></li>'); // data.data[x].images.low_resolution.url - URL of image, 306х306
-      //   		// 	// data.data[x].images.thumbnail.url - URL of image 150х150
-      //   		// 	// data.data[x].images.standard_resolution.url - URL of image 612х612
-      //   		// 	// data.data[x].link - Instagram post URL
-      //   		// }
-      //       // var str = 'viewCount : ' + data.videos.statistics.viewCount + ' ' + 'likeCount : ' + data.videos.statistics.likeCount;
-      //       // $.each(data.videos, function(index, value) {
-      //       //   callback(value.statistics.viewCount);
-      //       // })
-      //
-      //       // var str = '';
-      //       // for(var tmp in data){
-      //       //   callback(str);
-      //       //     str += data[tmp];
-      //       // }
-      //       // var obj = JSON.parse(str);
-      //       // callback(data.data.items[0].statistics.viewCount);
-      //       $.each(data.items, function( i, item ) {
-      //         callback(item.statistics.viewCount);
-      //       })
-      //       // callback('aaa');
-      //     }, // 요청 완료 시
-      //     error: function(data) {callback('bbb');}, // 요청 실패.
-      //     complete: function(data) {callback('ccc');} // 요청의 실패, 성공과 상관 없이 완료 될 경우 호출
-      // });
     };
 
     // Block and block menu descriptions
